a8c3015c77d3d3de667e4e6369ba904cb221f85b
Antonio Cucho <antonio.cucho@glr.pe>
2012-09-25 12:37:00 -0500

    Display suite

--- sites/all/themes/twitter_bootstrap/templates/html.tpl.php
+++ sites/all/themes/twitter_bootstrap/templates/html.tpl.php
@@ -1,19 +1,33 @@
 <!DOCTYPE html>
 <html xmlns:fb="http://ogp.me/ns/fb#" lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php print $head; ?>
-  <title><?php print $head_title; ?></title>
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
-  <!-- HTML5 element support for IE6-8 -->
-  <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
-</head>
-<body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <div id="fb-root"></div>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+    <!-- HTML5 element support for IE6-8 -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <script type="text/javascript">
+     
+      $(document).ready(function() {
+        $("#block-block-1").hide();
+        if((navigator.userAgent.match(/iPhone/i)) ||
+          (navigator.userAgent.match(/iPod/i)) ||
+          (navigator.userAgent.match(/android/i))) {
+          $("#block-block-1").show();
+        }
+      });
+      
+    </script>
+
+  </head>
+  <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
+    <div id="fb-root"></div>
     <script>(function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
@@ -21,8 +35,8 @@
       js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
-</body>
+    <?php print $page_top; ?>
+    <?php print $page; ?>
+    <?php print $page_bottom; ?>
+  </body>
 </html>
